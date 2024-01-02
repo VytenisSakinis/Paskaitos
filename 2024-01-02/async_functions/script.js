663e15034e6ab49e527f009da619bcd146d8e726
VytenisSakinis <vytenis.sakinis@gmail.com>
2024-01-02 10:28:00 +0200

    paskaitos

---
+++ 2024-01-02/async_functions/script.js
@@ -0,0 +1,49 @@
+// Error handlinimas
+try {
+    
+} catch (error) {
+    
+}
+
+// Pažadas
+
+// fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin")
+// .then((response)=>response.json())
+// .then((response)=>console.log(response))
+// .catch((error)=>console.log(error))
+
+
+// const pazadas = new Promise((resolve, reject) => {
+//     let isValidUrl = false;
+//     console.log("Prasidėjo")
+//     if(isValidUrl){
+//     setTimeout(() => resolve('Skambutis'), 1000)
+//     }
+//     else
+//         reject(new Error("Neteisingai nurodytas URL"))
+    
+//     console.log("Pasibaigė")
+// }).then((response) => console.log(response)).catch((error) => console.log(error))
+
+// function callServerSync()
+// {
+//     console.log("Prasidejo")
+//     setTimeout(() => {
+//         console.log("gauname atsakyma");}, 1000);
+
+//     console.log('baigesi')
+// }
+
+async function callServerAsync() {
+    console.log("Užklausa i serveri prasidėjo")
+    await new Promise((resolve, reject) => 
+        setTimeout(() => {
+            console.log("Gauname Atsakyma is serverio")
+            resolve();
+        }, 1000))
+
+        console.log("Uzklausa baigta")
+        return "atsakymas"
+}
+
+callServerAsync().then((response) => console.log(response))
--- cocktailAPI/script.js
+++ cocktailAPI/script.js
@@ -4,35 +4,75 @@ const ingredientSelectorElement = document.querySelector('#ingredientSelector')
 const drinkSearchElement = document.querySelector('#filterByText')
 const searchButtonElement = document.querySelector('#searchButton')
 const feelingLuckyButtonElement = document.querySelector('#feelingLuckyButton')
+const containerHTML = document.querySelector('.drinks')
+const categoriesArray = [], drinksArray = []
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
-.then((response) => response.json())
-.then((response) => printDynamicOptionHTML(response, categorySelectElement, "strCategory"))
+async function fillSelectElements() {
+    await fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list")
+    .then((response) => response.json())
+    .then((response) => {
+        printDynamicOptionHTML(response, categorySelectElement, "strCategory")
+        categoriesArray.push(... response.drinks.map((value) => value.strCategory))
+})
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list")
-.then((response) => response.json())
-.then((response) => printDynamicOptionHTML(response, glassSelectorElement, "strGlass"))
+    await fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list")
+    .then((response) => response.json())
+    .then((response) => {
+        printDynamicOptionHTML(response, glassSelectorElement, "strGlass")
+    })
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
-.then((response) => response.json())
-.then((response) => printDynamicOptionHTML(response, ingredientSelectorElement, "strIngredient1"))
-
-
-
+    await fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+    .then((response) => response.json())
+    .then((response) => printDynamicOptionHTML(response, ingredientSelectorElement, "strIngredient1"))
+}
 
 function printDynamicOptionHTML(response, element, str)
 {
     let dynamicHTML = ''
     const categoryArray = []
     for(let value of response.drinks)
-    {
-        categoryArray.push(value[str])
-    }
+                categoryArray.push(value[str])
 
-    for(let index in categoryArray) dynamicHTML += `<option>${categoryArray[index]}</option>`
+    for(let index in categoryArray) 
+                dynamicHTML += `<option>${categoryArray[index]}</option>`
 
     element.innerHTML = dynamicHTML
-
 }
 
+async function getAllDrinks() {
+    // https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink
+    for(const category of categoriesArray)
+    {
+        let dynamicUrl = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category.replaceAll(" ", "_")}`;
+        const response = await fetch(dynamicUrl);
+        const answerFromServer = await response.json();
+        for(const drink of answerFromServer.drinks)
+        drinksArray.push(drink)
+    }
+    
+}
 
+function generateDrinksHTML(drinks) {
+    let dynamicHTML = ''
+    for(const drink of drinks)
+    {
+        dynamicHTML += `
+        <div class="drink">
+            <img
+              src="${drink.strDrinkThumb}"
+            />
+            <p class="title">${drink.strDrink}</p>
+          </div>`
+    }
+    containerHTML.innerHTML = dynamicHTML
+}
+
+async function initialization()
+{
+    await fillSelectElements()
+    console.log(categoriesArray);
+    await getAllDrinks();
+    console.log(drinksArray)
+    generateDrinksHTML(drinksArray)
+}
+initialization()
