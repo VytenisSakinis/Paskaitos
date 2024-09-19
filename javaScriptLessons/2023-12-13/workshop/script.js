7fa3d771359c6c88fe284f7d32b6d7409f490edb
VytenisSakinis <vytenis.sakinis@gmail.com>
2024-09-19 22:35:58 +0300

    Upgraded the BMI calculator

--- javaScriptLessons/2023-12-13/workshop/script.js
+++ javaScriptLessons/2023-12-13/workshop/script.js
@@ -3,7 +3,9 @@ weight = document.querySelector(".weight"),
 calculate = document.querySelector(".calculate"),
 reset = document.querySelector(".reset"),
 result = document.querySelector(".results"),
-display = document.querySelector(".display-none")
+display = document.querySelector(".display-none"),
+genderInput = document.querySelector("#genderSelector"),
+perfectWeight = document.querySelector(".perfectWeight")
 
 display.style.display = "none";
 
@@ -13,7 +15,7 @@ function calBMI(e) {
     e.preventDefault()
 
     let heightValue = height.value
-
+    let gender = genderInput.value
     let weightValue = weight.value
 
     if(!heightValue || isNaN(heightValue)) return result.innerHTML = "Įveskite tinkamą aukštį";
@@ -22,6 +24,8 @@ function calBMI(e) {
         let heightMeters = heightValue/100
 
         let bmi = (weightValue/Math.pow(heightMeters, 2)).toFixed(2)
+        let maleWeight = (50 + (0.91 * (heightValue - 152.4))).toFixed(2)
+        let femaleWeight = (45.5 + (0.91 * (heightValue - 152.4))).toFixed(0) 
         
         display.style.display = "block";
 
@@ -37,6 +41,12 @@ function calBMI(e) {
         if(bmi > 30) {
         showResults(`Obese - ${bmi}`, "red")
         }
+        if(gender === "Male") {
+            perfectWeightDisplay(maleWeight)
+        }
+        if(gender === "Female") {
+            perfectWeightDisplay(femaleWeight)
+        }
     }
 
 }
@@ -44,6 +54,10 @@ function calBMI(e) {
 function showResults(value, color){
     result.style.backgroundColor = color;
     return result.innerHTML = value
+}
+
+function perfectWeightDisplay(value) {
+    perfectWeight.innerHTML = `Your perfect weight should be ~${value}`
 }
 
 reset.addEventListener("click", resetBMI)
